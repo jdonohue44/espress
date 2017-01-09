bb0ae6e2c1d001c12d07107f71188363128946f8
jdonohue44 <jared.donohue@gmail.com>
2017-01-09 17:22:45 -0500

    update

--- scripts/espress.py
+++ scripts/espress.py
@@ -1,7 +1,7 @@
-def create_dict(artists):
+def create_dict(interests):
 	dict = {}
-	for artist in artists:
-		dict[artist] = {}
+	for i in interests:
+		dict[i] = {}
 	return dict
 
 import MySQLdb
@@ -13,8 +13,21 @@ import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-emails = []
-test_file= open('/home/ec2-user/espress/dbtest.log','a')
+
+# Feed/Parse Variables
+interests = []
+stock_abrevs = []
+google_news_rss_url = 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q='
+google_finance_feed = 'http://www.google.com/finance/info?q=NASDAQ:'
+
+#Mail Variables
+source =  'espressmorningnews@gmail.com'
+dest   =  ''
+name   =  ''
+
+# sd = stocks dictionary --> {'MSFT': 63.23, 'FB': 126.11, 'AMZN':788.99}
+sd = {}
+
 db = MySQLdb.connect(host="jd-db-instance.csuhsua8cx8a.us-east-1.rds.amazonaws.com",    # your host, usually localhost
                      user="jdonohue44",         # your username
                      passwd="dubaiguy$$",  # your password
@@ -29,94 +42,75 @@ cur.execute("SELECT * FROM USERS")
 
 # print each user in DB
 for user in cur.fetchall():
-    # test_file.write(str(row[0]) + "\t" + row[1] + "\t" + row[2] + "\n")
+	uid  = user[0]
+	name = user[1]
+	dest = user[2]
 	cur.execute("""
 	SELECT INTERESTS.Interest FROM USER_INTERESTS
 	INNER JOIN USERS ON USERS.ID = USER_INTERESTS.User_ID
 	INNER JOIN INTERESTS ON INTERESTS.ID = USER_INTERESTS.Interest_ID
 	where USERS.ID = %s;
-	""", (user[0],))
+	""", (uid,))
 	for interest in cur.fetchall():
-		test_file.write(str(user[1]) + "\t" + str(interest[0]) +"\n")
+		interests.append(interest[0])
+
+	# iid = interest information dictionary --> {'interest':{'query':'','title':'','link':'','date':''}}
+	iid = create_dict(interests)
+
+	# Put Queries into Artist dictionary
+	for i in iid:
+		query = ''
+		words = i.lower().split()
+		for num in range(0,len(words)-1):
+			query += words[num] + "+"
+		query += words[len(words)-1]
+		iid[i]['query'] = query
+
+	# Put news info into Artist dictionary
+	for i in iid:
+		d = feedparser.parse(google_news_rss_url + iid[i]['query'] + '&output=rss')
+		iid[a]['title'] = d['entries'][1]['title']
+		iid[a]['link']  = d['entries'][1]['link']
+		iid[a]['date']  = d['entries'][1]['published']
+
+
+	for stock in stock_abrevs:
+		r = requests.get(google_finance_feed + stock)
+		json_response = json.loads(r.text[4:])
+		curr_price = json_response[0]['l_cur']
+		sd[stock] = curr_price
+
+	# Mail Service
+	message = MIMEMultipart()
+	message['From'] = source
+	message['To'] = dest
+	message['Subject'] = 'Good Morning from Espress, Today is ' + time.strftime("%d/%m/%Y")
+
+	f1 = open('/home/ec2-user/espress/html/template1.html','r')
+	f2 = open('/home/ec2-user/espress/html/template2.html','r')
+	f3 = open('/home/ec2-user/espress/html/template3.html','r')
+	log_file = open('/home/ec2-user/espress/logs.log','a')
+
+	html = f1.read()
+	for i in iid:
+		html += "<a href ='" + iid[i]['link'] + "'</a>" + iid[i]['title'] + "<br /><br />"
+	html += f2.read()
+	for s in sd:
+		html += s + " price per share: $" + sd[s] + "<br /><br />"
+	html += f3.read()
+
+	message.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
+
+	smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+	smtp_server.starttls()
+	smtp_server.login(source, '5638JabroniStreet**')
+
+	try:
+	   smtp_server.sendmail(source, mailing_list, message.as_string())
+	   smtp_server.quit()
+	   log_file.write("Successfully sent email -- " + time.strftime("%m-%d-%Y %H:%M"))
+	except smtplib.SMTPException:
+	   print("Error: unable to send email")
+	   log_file.write("ERROR sending email to" + dest + " -- " + time.strftime("%m-%d-%Y %H:%M"))
+
 db.close()
-
-
-
-
-
-
-
-
-
-#
-# # Feed/Parse Variables
-# artists = ['The Growlers', 'Martin Scorsese', 'Wes Anderson', 'Richard Linklater']
-# stock_abrevs = ['MSFT','FB','AMZN']
-# google_news_rss_url = 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q='
-# google_finance_feed = 'http://www.google.com/finance/info?q=NASDAQ:'
-#
-# #Mail Variables
-# source =  'espressmorningnews@gmail.com'
-# mailing_list = ['jared.donohue@gmail.com']
-#
-# # aid = artist information dictionary --> {'artist':{'query':'','title':'','link':'','date':''}}
-# aid = create_dict(artists)
-#
-# # sd = stocks dictionary --> {'MSFT': 63.23, 'FB': 126.11, 'AMZN':788.99}
-# sd = {}
-#
-# # Put Queries into Artist dictionary
-# for a in aid:
-# 	query = ''
-# 	words = a.lower().split()
-# 	for num in range(0,len(words)-1):
-# 		query += words[num] + "+"
-# 	query += words[len(words)-1]
-# 	aid[a]['query'] = query
-#
-# # Put news info into Artist dictionary
-# for a in aid:
-# 	d = feedparser.parse(google_news_rss_url + aid[a]['query'] + '&output=rss')
-# 	aid[a]['title'] = d['entries'][1]['title']
-# 	aid[a]['link']  = d['entries'][1]['link']
-# 	aid[a]['date']  = d['entries'][1]['published']
-#
-#
-# for stock in stock_abrevs:
-# 	r = requests.get(google_finance_feed + stock)
-# 	json_response = json.loads(r.text[4:])
-# 	curr_price = json_response[0]['l_cur']
-# 	sd[stock] = curr_price
-#
-# # Mail Service
-# message = MIMEMultipart()
-# message['From'] = source
-# message['To'] = ", ".join(mailing_list)
-# message['Subject'] = 'Good Morning from Espress, Today is ' + time.strftime("%d/%m/%Y")
-#
-# f1 = open('/home/ec2-user/espress/html/template1.html','r')
-# f2 = open('/home/ec2-user/espress/html/template2.html','r')
-# f3 = open('/home/ec2-user/espress/html/template3.html','r')
-# log_file = open('/home/ec2-user/espress/logs.log','a')
-#
-# html = f1.read()
-# for a in aid:
-# 	html += "<a href ='" + aid[a]['link'] + "'</a>" + aid[a]['title'] + "<br /><br />"
-# html += f2.read()
-# for s in sd:
-# 	html += s + " price per share: $" + sd[s] + "<br /><br />"
-# html += f3.read()
-#
-# message.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
-#
-# smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
-# smtp_server.starttls()
-# smtp_server.login(source, '5638JabroniStreet**')
-#
-# try:
-#    smtp_server.sendmail(source, mailing_list, message.as_string())
-#    smtp_server.quit()
-#    log_file.write("Successfully sent email -- " + time.strftime("%m-%d-%Y %H:%M"))
-# except smtplib.SMTPException:
-#    print("Error: unable to send email")
-#    log_file.write("ERROR sending email -- " + time.strftime("%m-%d-%Y %H:%M"))
