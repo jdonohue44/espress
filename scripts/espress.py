2dd6bc506879d7fbaec4fc8aff8311230885f3c5
jdonohue44 <jared.donohue@gmail.com>
2017-02-07 13:16:44 -0500

    update

--- scripts/espress.py
+++ scripts/espress.py
@@ -83,7 +83,7 @@ for user in cur.fetchall():
 	html = f1.read()
 	for i in iid:
 		html += "<b>(" + iid[i]['date'] + ")</b> " + " <b>" + iid[i]['source'] + "</b>"
-		html += "<a href ='" + iid[i]['link'] + "'> " + iid[i]['title'] + "</a>"
+		html += "<a href ='" + iid[i]['link'] + "'> " + iid[i]['title'] + "</a><br />"
 		html += "<br /><br />"
 	html += f2.read()
 
