0e0f88c2324fe5aafb42a71e639b81cfb5161173
jdonohue44 <jared.donohue@gmail.com>
2017-02-19 11:28:35 -0500

    update

--- scripts/espress.py
+++ scripts/espress.py
@@ -90,10 +90,10 @@ for user in cur.fetchall():
 
 	html = f1.read()
 	for i in iid:
-		html += "<tr><td style='padding: 20px; border-color: #dbdbdb; border-style: solid; border-width: 4px 4px 0px 4px'>"
-		html += "<div style='text-align:center;'><b style='font-size:16px;'>" + iid[i]['title'] + "</b></div>"
+		html += "<tr><td style='padding: 20px;'>"
+		html += "<div style='text-align:center; padding: 10px; border-style: solid; border-radius: 4px; border-width: 1px; border-color: #c5c5c5;'><b style='font-size:16px;'>" + iid[i]['title'] + "</b></div>"
 		html += "<p style='text-align: center; font-size: 11px; margin-top: 4px;'>Retrieved from " + iid[i]['source'] + " on " + iid[i]['date'] + ", based on your interest in <b>" + i + "</b></p>"
-		html += "<div style='text-align:right; padding-top:12px'><a style='padding:8px; font-size: 16px;' href ='" + iid[i]['link'] + "'>Read Article</a></div>"
+		html += "<div style='text-align:right;'><a style='padding:8px; font-size: 16px;' href ='" + iid[i]['link'] + "'>Read Article</a></div>"
 		html += "</td></tr>"
 	html += f2.read()
 
