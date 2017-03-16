e2e3098b1a34e7039b141dd8373777c80c665636
jdonohue44 <jared.donohue@gmail.com>
2017-03-15 21:29:49 -0400

    update

--- scripts/espress.py
+++ scripts/espress.py
@@ -90,7 +90,7 @@ for user in cur.fetchall():
 
 	# html = f1.read()
 	for i in iid:
-		log_file.write("ID: " + user[0] +"\tName: " + user[1] + "\tEmail: " + user[2] + "\tInterest: " + i + ".\n")
+		log_file.write("ID: " + int(user[0]) +"\tName: " + user[1] + "\tEmail: " + user[2] + "\tInterest: " + i + ".\n")
 	log_file.write("\n\n\n")
 	# 	html += "<tr><td style='padding: 20px;'>"
 	# 	html += "<div style='text-align:center; padding: 10px;'><b style='font-weight: 100; font-size: 24px; font-family: sans-serif;'>" + iid[i]['title'] + "</b></div>"
