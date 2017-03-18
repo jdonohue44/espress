47e51af13e89b7488fc99e754c4487bc942e0552
jdonohue44 <jared.donohue@gmail.com>
2017-03-17 22:20:43 -0400

    update

--- scripts/espress.py
+++ scripts/espress.py
@@ -1,9 +1,3 @@
-def create_dict(interests):
-	dict = {}
-	for i in interests:
-		dict[i] = {}
-	return dict
-
 import MySQLdb
 import feedparser
 import requests
@@ -13,56 +7,51 @@ import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+def create_dict(interests):
+	dict = {}
+	for i in interests:
+		dict[i] = {}
+	return dict
 
-# Feed/Parse Global
-google_news_rss_url = 'https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q='
+def get_users_from_DB():
+	# db = MySQLdb.connect(host="jd-db-instance.csuhsua8cx8a.us-east-1.rds.amazonaws.com",
+	#                      user="jdonohue44",
+	#                      passwd="dubaiguy$$",
+	#                      db="Dubai")
+	# cur = db.cursor()
+	# cur.execute("SELECT * FROM USERS")
+	# return cur.fetchall
+	return (1, "Jared", "jared.donohue@gmail.com")
 
-#Mail Variables
-source =  'espressmorningnews@gmail.com'
-dest   =  ''
-name   =  ''
+def get_user_interests(uid):
+		# cur.execute("""
+		# SELECT INTERESTS.Interest FROM USER_INTERESTS
+		# INNER JOIN USERS ON USERS.ID = USER_INTERESTS.User_ID
+		# INNER JOIN INTERESTS ON INTERESTS.ID = USER_INTERESTS.Interest_ID
+		# where USERS.ID = %s;
+		# """, (uid,))
+		# return cur.fetchall()
+		return ("")
 
-db = MySQLdb.connect(host="jd-db-instance.csuhsua8cx8a.us-east-1.rds.amazonaws.com",    # your host, usually localhost
-                     user="jdonohue44",         # your username
-                     passwd="dubaiguy$$",  # your password
-                     db="Dubai")        # name of the data base
+users = get_users_from_DB()
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
-
-# Use all the SQL you like
-cur.execute("SELECT * FROM USERS")
-
-# each user in DB
-for user in cur.fetchall():
+for user in users:
 	uid  = user[0]
 	name = user[1]
 	dest = user[2]
-	interests = []
+	# interests = []
+	# interest_rows = get_user_interests(uid)
+	interests = ["Spotify", "Bill Withers", "Coffee", "March Madness", "Boston"]
 
-	# get this users interests from the join table USER_INTERESTS
-	cur.execute("""
-	SELECT INTERESTS.Interest FROM USER_INTERESTS
-	INNER JOIN USERS ON USERS.ID = USER_INTERESTS.User_ID
-	INNER JOIN INTERESTS ON INTERESTS.ID = USER_INTERESTS.Interest_ID
-	where USERS.ID = %s;
-	""", (uid,))
+	# get the interest name (interest[0])
+	# for interest in interest_rows:
+	# 	interests.append(interest[0])
 
-	# if this user has no interests (rare) then skip to next user
-	all_interests = cur.fetchall()
-	if(len(all_interests) == 0):
-		continue;
-
-	# get all the users interests
-	for interest in all_interests:
-		interests.append(interest[0])
-
-	# iid = interest information dictionary --> {'interest':{'query':'','title':'','link':'','date':''}}
-	iid = create_dict(interests)
+	# create interest information dictionary --> {'interest':{'query':'','title':'','link':'','date':''}}
+	interest_info_dictonary = create_dict(interests)
 
 	# Put Queries into interest information dictionary
-	for i in iid:
+	for i in interest_info_dictonary:
 		query = ''
 		words = i.lower().split()
 		for num in range(0,len(words)-1):
@@ -72,43 +61,44 @@ for user in cur.fetchall():
 
 	# Put news info into interest information dictionary
 	for i in iid:
-		d = feedparser.parse(google_news_rss_url + iid[i]['query'] + '&output=rss')
-		iid[i]['link']   = d['entries'][1]['link']
-		iid[i]['date']   = d['entries'][1]['published'][:-13]
-		iid[i]['source'] = d['entries'][1]['title'].split("-")[-1]
-		iid[i]['title']  = d['entries'][1]['title'][:-(len(iid[i]['source'])+2)]
+		d = feedparser.parse('https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&q='+ iid[i]['query'] + '&output=rss')
+		# iid[i]['link']   = d['entries'][1]['link']
+		# iid[i]['date']   = d['entries'][1]['published'][:-13]
+		# iid[i]['source'] = d['entries'][1]['title'].split("-")[-1]
+		# iid[i]['title']  = d['entries'][1]['title'][:-(len(iid[i]['source'])+2)]
+		print(d)
 
 	# Mail Service
-	message = MIMEMultipart()
-	message['From'] = source
-	message['To'] = dest
-	message['Subject'] = 'Your Espress Newsletter'
-
-	f1 = open('/home/ec2-user/espress/html/template1.html','r')
-	f2 = open('/home/ec2-user/espress/html/template2.html','r')
-	log_file = open('/home/ec2-user/espress/logs.log','a')
-
-	html = f1.read()
-	for i in iid:
-		html += "<tr><td style='padding: 20px;'>"
-		html += "<div style='text-align:center; padding: 10px;'><b style='font-weight: 100; font-size: 24px; font-family: sans-serif;'>" + iid[i]['title'] + "</b></div>"
-		html += "<p style='text-align: center; font-size: 11px; margin-top: 4px;'>Retrieved from " + iid[i]['source'] + " on " + iid[i]['date'] + ", based on your interest in <b>" + i + "</b></p>"
-		html += "<div style='text-align:right;'><a style='padding:6px; font-size: 16px; text-decoration: underline; margin-right: 30px;' href ='" + iid[i]['link'] + "'>Read Article</a></div>"
-		html += "</td></tr>"
-	html += f2.read()
-
-	message.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
-
-	smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
-	smtp_server.starttls()
-	smtp_server.login(source, '5638JabroniStreet**')
-
-	try:
-	   smtp_server.sendmail(source, dest, message.as_string())
-	   smtp_server.quit()
-	   log_file.write("Successfully sent email -- " + time.strftime("%m-%d-%Y %H:%M") + "\n")
-	except smtplib.SMTPException:
-	   print("Error: unable to send email")
-	   log_file.write("ERROR sending email to" + dest + " -- " + time.strftime("%m-%d-%Y %H:%M") + "\n")
+	# message = MIMEMultipart()
+	# message['From'] = 'espressmorningnews@gmail.com'
+	# message['To'] = dest
+	# message['Subject'] = 'Your Espress Newsletter'
+	#
+	# f1 = open('/home/ec2-user/espress/html/template1.html','r')
+	# f2 = open('/home/ec2-user/espress/html/template2.html','r')
+	# log_file = open('/home/ec2-user/espress/logs.log','a')
+	#
+	# html = f1.read()
+	# for i in iid:
+	# 	html += "<tr><td style='padding: 20px;'>"
+	# 	html += "<div style='text-align:center; padding: 10px;'><b style='font-weight: 100; font-size: 24px; font-family: sans-serif;'>" + iid[i]['title'] + "</b></div>"
+	# 	html += "<p style='text-align: center; font-size: 11px; margin-top: 4px;'>Retrieved from " + iid[i]['source'] + " on " + iid[i]['date'] + ", based on your interest in <b>" + i + "</b></p>"
+	# 	html += "<div style='text-align:right;'><a style='padding:6px; font-size: 16px; text-decoration: underline; margin-right: 30px;' href ='" + iid[i]['link'] + "'>Read Article</a></div>"
+	# 	html += "</td></tr>"
+	# html += f2.read()
+	#
+	# message.attach(MIMEText(html.encode('utf-8'), 'html', 'utf-8'))
+	#
+	# smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+	# smtp_server.starttls()
+	# smtp_server.login(source, '5638JabroniStreet**')
+	#
+	# try:
+	#    smtp_server.sendmail(source, dest, message.as_string())
+	#    smtp_server.quit()
+	#    log_file.write("Successfully sent email -- " + time.strftime("%m-%d-%Y %H:%M") + "\n")
+	# except smtplib.SMTPException:
+	#    print("Error: unable to send email")
+	#    log_file.write("ERROR sending email to" + dest + " -- " + time.strftime("%m-%d-%Y %H:%M") + "\n")
 
 db.close()
